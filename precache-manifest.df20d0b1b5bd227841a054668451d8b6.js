a92344bc070fe681938c4672a386f533732780ba
Vitalik Prac <mastersasori007@gmail.com>
2020-11-01 19:47:26 +0200

    Updates

---
+++ precache-manifest.df20d0b1b5bd227841a054668451d8b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a99d166055705c6b840a744c38c2c37",
+    "url": "/cryptlab2/index.html"
+  },
+  {
+    "revision": "9325021d2df0bc9ea76c",
+    "url": "/cryptlab2/static/css/2.fb7da10c.chunk.css"
+  },
+  {
+    "revision": "92f4422ebd6e392eb392",
+    "url": "/cryptlab2/static/css/main.18bf0348.chunk.css"
+  },
+  {
+    "revision": "9325021d2df0bc9ea76c",
+    "url": "/cryptlab2/static/js/2.38924a72.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cryptlab2/static/js/2.38924a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f4422ebd6e392eb392",
+    "url": "/cryptlab2/static/js/main.9db730a3.chunk.js"
+  },
+  {
+    "revision": "9139867a82b4115e83fe",
+    "url": "/cryptlab2/static/js/runtime-main.2442b238.js"
+  }
+]);
